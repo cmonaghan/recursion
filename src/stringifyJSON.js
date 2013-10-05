319e1991889b19a4c7caec6e49b11c7d469cd75f
Christian Monaghan <christian.monaghan@gmail.com>
2013-10-05 14:20:25 -0700

    Added ability to view code result in HTML browser

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,6 +4,29 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
-  //var string = obj.toString();
-  //return string;
+  var jsonResult = '';
+  for(var key in obj) {
+  	jsonResult += key.toString() + ', ';
+  }
+  return jsonResult;
 };
+
+
+// ------------------------------------
+
+// An optional function input for JSON.stringify
+/*function censor(key, value) {
+  if (typeof(value) == "string") {
+    return undefined;
+  }
+  return value;
+}*/
+
+// Run my function vs theirs
+var foo = {foundation: "Mozilla", model: "box", week: 45, transport: "car", month: 7};
+var jsonStringTheirs = JSON.stringify(foo);
+var jsonStringMine = stringifyJSON(foo);
+
+// Print responses
+document.write('This is what it should print:<br />' + jsonStringTheirs + '<p></p>');
+document.write('This is what mine prints:<br />' + jsonStringMine);
